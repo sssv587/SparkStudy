f47d077c3199ed7fd3bcc5ba0d3d76ca13a63975
sssv587 <1092663295@qq.com>
2021-03-16 01:51:30 +0800

    Test

---
+++ SparkCore/src/main/scala/com/futurebytedance/sparkcore/Test.scala
@@ -0,0 +1,11 @@
+package com.futurebytedance.sparkcore
+
+/**
+ * @author yuhang.sun 2021/3/16 - 1:27
+ * @version 1.0
+ */
+object Test {
+  def main(args: Array[String]): Unit = {
+    println("Hello Spark")
+  }
+}
