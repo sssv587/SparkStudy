bb1861ffb500bb548b4768cfdd87d55b34778aaa
sssv587 <1092663295@qq.com>
2021-03-23 01:19:31 +0800

    RDD-Transform-leftOuterJoin

---
+++ SparkCore/src/main/scala/com/futurebytedance/sparkcore/rdd/oper/Spark22_RDD_Operator_Transform.scala
@@ -0,0 +1,28 @@
+package com.futurebytedance.sparkcore.rdd.oper
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+
+/**
+ * @author yuhang.sun 2021/3/23 - 1:15
+ * @version 1.0
+ *          RDD转换算子-leftOuterJoin算子和rightOuterJoin
+ */
+object Spark22_RDD_Operator_Transform {
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+    val sc: SparkContext = new SparkContext(sparkConf)
+
+    //TODO 算子-leftOuterJoin算子和rightOuterJoin算子
+    val rdd1: RDD[(String, Int)] = sc.makeRDD(List(("a", 1), ("b", 2)), 2)
+    val rdd2: RDD[(String, Int)] = sc.makeRDD(List(("a", 4), ("b", 5), ("c", 3)))
+
+
+    val leftJoinRDD: RDD[(String, (Int, Option[Int]))] = rdd1.leftOuterJoin(rdd2)
+    val rightJoinRDD: RDD[(String, (Option[Int], Int))] = rdd1.rightOuterJoin(rdd2)
+    leftJoinRDD.collect().foreach(println)
+    rightJoinRDD.collect().foreach(println)
+
+    sc.stop()
+  }
+}
