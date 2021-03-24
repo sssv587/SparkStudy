9fcaf4a95ff5535c4c9d6a14be591793acc1958d
sssv587 <1092663295@qq.com>
2021-03-24 23:20:17 +0800

    RDD-Action-takeOrdered

--- SparkCore/src/main/scala/com/futurebytedance/sparkcore/rdd/action/Spark07_RDD_Operator_Action.scala
+++ SparkCore/src/main/scala/com/futurebytedance/sparkcore/rdd/action/Spark07_RDD_Operator_Action.scala
@@ -7,6 +7,12 @@ import org.apache.spark.rdd.RDD
  * @author yuhang.sun 2021/3/24 - 23:16
  * @version 1.0
  *          RDD行动算子-takeOrdered
+ *
+ * 函数签名
+ * def takeOrdered(num: Int)(implicit ord: Ordering[T]): Array[T]
+ *
+ * 函数说明
+ * 返回该 RDD 排序后的前 n 个元素组成的数组
  */
 object Spark07_RDD_Operator_Action {
   def main(args: Array[String]): Unit = {
