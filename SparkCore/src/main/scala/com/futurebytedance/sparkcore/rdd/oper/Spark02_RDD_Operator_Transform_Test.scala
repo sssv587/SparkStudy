56660ca860034e38e0b88992e431ca99b86e8278
sssv587 <1092663295@qq.com>
2021-03-24 22:56:34 +0800

    RDD-Transform-mapPartitions

--- SparkCore/src/main/scala/com/futurebytedance/sparkcore/rdd/oper/Spark02_RDD_Operator_Transform_Test.scala
+++ SparkCore/src/main/scala/com/futurebytedance/sparkcore/rdd/oper/Spark02_RDD_Operator_Transform_Test.scala
@@ -24,6 +24,17 @@ object Spark02_RDD_Operator_Transform_Test {
 
     mapPartitionsRDD.collect().foreach(println)
 
+    //map 和 mapPartitions 的区别？
+    //1.数据处理角度
+    //map算子是分区内一个数据一个数据的执行，类似于串行操作。而mapPartitions算子是以分区为单位进行批处理操作
+    //2.功能的角度
+    //map算子的主要目的是将数据源中的数据进行转换和改变。但是不会减少或增多数据
+    //mapPartitions算子需要传递一个迭代器，返回一个迭代器，没有要求元素的个数保持不变，所以可以增加或减少数据
+    //3.性能的角度
+    //map算子因为类似于串行操作，所以性能比较低，而mapPartitions算子类似于批处理，性能较高
+    //但是mapPartitions算子会长时间占用内存，那么这样会导致内存可能不够用，出现内存溢出的错误。所以在内存有限的情况下，不推荐使用。
+
+
     sc.stop()
   }
 }
