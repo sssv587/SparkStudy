274ec15fc9a983562aa0ab4f7c9c28575d90f3a1
sssv587 <1092663295@qq.com>
2021-04-08 00:58:54 +0800

    SparkStreaming-Output

---
+++ SparkStreaming/src/main/scala/com/futurebytedance/sparkstreaming/SparkStreaming07_Output.scala
@@ -0,0 +1,35 @@
+package com.futurebytedance.sparkstreaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+
+/**
+ * @author yuhang.sun 2021/4/8 - 0:53
+ * @version 1.0
+ *          SparkStreaming-Output
+ */
+object SparkStreaming07_Output {
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
+    val ssc: StreamingContext = new StreamingContext(sparkConf, Seconds(3))
+    ssc.checkpoint("cp")
+
+    val lines: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 9999)
+
+    val wordToOne: DStream[(String, Int)] = lines.map((_, 1))
+
+
+    val windowDS: DStream[(String, Int)] = wordToOne.reduceByKeyAndWindow(
+      (x: Int, y: Int) => x + y,
+      (x: Int, y: Int) => x - y,
+      Seconds(9), Seconds(3))
+
+    //SparkStreaming如果没有输出操作，那么会提示错误
+    //windowDS.print()
+
+    ssc.start()
+
+    ssc.awaitTermination()
+  }
+}
